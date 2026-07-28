a96a8f762f60fbbcc58a3ec4f7c0f32c2ba46461
your-name <your-email>
2026-07-28 10:24:40 +0530

    clkb function

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("hy")
+// console.log("hy")
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function root(a,b){
+//     return Math.sqrt(a)+Math.sqrt(b);
+// }
+
+// console.log(root(9,16))
+
+// (() =>{
+//     console.log("heyyy....using IIFE")
+// })
+
+let a = 56;
+// console.log(typeof(a));
+
+
+if(a>40){
+    let a = 55;
+    console.log(a);
+}
+console.log(a);
+
+function login(error,msg){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(msg);
+    }
+}
+
+function loginhandler(username,password,clbk){
+    if(username=="shivam" && password=="12345"){
+        clbk(null,"login success")
+    }
+    else{
+        clbk("username or password is incorrect",null)
+    }
+}
+
+loginhandler("shivm","12345",login);
+
