ff945d3a5d27c69bffdd5dfff0f9cf25cb92c97f
Shubh Mishra <shubhmishra5859@gmail.com>
2026-08-18 10:14:58 +0530

    commit 18/8 js

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-// console.log("hy")
+// // console.log("hy")
 
-// function sum(a,b){
-//     return a+b;
+// // function sum(a,b){
+// //     return a+b;
+// // }
+
+// // function root(a,b){
+// //     return Math.sqrt(a)+Math.sqrt(b);
+// // }
+
+// // console.log(root(9,16))
+
+// // (() =>{
+// //     console.log("heyyy....using IIFE")
+// // })
+
+// let a = 56;
+// // console.log(typeof(a));
+
+
+// if(a>40){
+//     let a = 55;
+//     console.log(a);
+// }
+// console.log(a);
+
+// function login(error,msg){
+//     if(error){
+//         console.log(error);
+//     }
+//     else{
+//         console.log(msg);
+//     }
 // }
 
-// function root(a,b){
-//     return Math.sqrt(a)+Math.sqrt(b);
+// function loginhandler(username,password,clbk){
+//     if(username=="shivam" && password=="12345"){
+//         clbk(null,"login success")
+//     }
+//     else{
+//         clbk("username or password is incorrect",null)
+//     }
 // }
 
-// console.log(root(9,16))
+// loginhandler("shivm","12345",login);
 
-// (() =>{
-//     console.log("heyyy....using IIFE")
-// })
+const container = document.getElementById('container');
+const button = document.getElementById('btn');
+h1.innerText = 'ABES Engineering College';
+// console.log(button)
+// console.log(container)
 
-let a = 56;
-// console.log(typeof(a));
-
-
-if(a>40){
-    let a = 55;
-    console.log(a);
+ function ping(){
+    try{
+    // alert('server ping')
+    container.innerHTML = '<h2>Welcome to DOM</h2>'
+    container.appendChild(h1);
+}catch(e){
+    loader.innerHTML=<h2 style=color:red>Error in loading data</h2>
 }
-console.log(a);
+ }
 
-function login(error,msg){
-    if(error){
-        console.log(error);
-    }
-    else{
-        console.log(msg);
-    }
-}
-
-function loginhandler(username,password,clbk){
-    if(username=="shivam" && password=="12345"){
-        clbk(null,"login success")
-    }
-    else{
-        clbk("username or password is incorrect",null)
-    }
-}
-
-loginhandler("shivm","12345",login);
-
+ button.addEventListener('click',ping);
